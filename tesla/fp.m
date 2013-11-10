b662e6af5d2de516f15b3ff538d072bad0a98917
Hampus Fristedt <hampus.fristedt93@gmail.com>
2013-11-10 17:32:00 +0100

    Sort of done with 2/3 parts.

--- tesla/fp.m
+++ tesla/fp.m
@@ -1,4 +1,4 @@
 function f = fp(v, rz)
   global C a
-  f = [C * integral(@(fi) (rz(2)      .* cos(fi)) ./ sqrt((rz(1) - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + rz(2).^2), -pi, pi)
-       C * integral(@(fi) (a - rz(1)  .* cos(fi)) ./ sqrt((rz(1) - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + rz(2).^2), -pi, pi)];
+  f = [C * integral(@(fi) (rz(2)      .* cos(fi)) ./ sqrt((rz(1) - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + rz(2).^2).^3, 0, pi/2);
+       C * integral(@(fi) (a - rz(1)  .* cos(fi)) ./ sqrt((rz(1) - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + rz(2).^2).^3, 0, pi/2)];
--- tesla/tesla.m
+++ tesla/tesla.m
@@ -1,22 +1,35 @@
 % Konstanter.
 clear all
-global a I0 ur C 
+global a C 
 a  = 0.1;
 I0 = 2.0;
 ur = 40000;
 C  = ur * I0 * a * 1e-7;
-r0 = 0.5 * a;
-z0 = 0;
 % mopt=odeset('RelTol',1.E-6);
 
 % Funktioner.
-% integral(@(fi) )
-% tmp = @(fi) z .* sin(fi) ./ sqrt((r - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + z.^2)
-% tmp([0: 0.1: pi])
-% br = @(r, z) C * integral(@(fi) z .* sin(fi) ./ sqrt((r - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + z.^2), -pi, pi);
-% bz = @(r, z) C * integral(@(fi) (a - r .* cos(fi)) ./ sqrt((r - a.*cos(fi)).^2 + a.^2.*sin(fi).^2 + z.^2), -pi, pi);
-[V, RZ] = ode45(@fp, [-1e5 1e5], [r0 z0]);
+r0 = 0.2 * a;
+z0 = 0;
+[V, RZ] = ode45(@fp, [-pi 5*pi], [r0 z0]);
 r = RZ(:,1)
 z = RZ(:,2)
-subplot(2, 2, 1); plot(V, r, V, z, '--');
-subplot(2, 2, 2); plot(r, z);
+hold on;
+plot(r, z);
+
+r0 = 0.3 * a;
+[V, RZ] = ode45(@fp, [-pi 5*pi], [r0 z0]);
+r = RZ(:,1)
+z = RZ(:,2)
+plot(r, z, '-');
+
+r0 = 0.4 * a;
+[V, RZ] = ode45(@fp, [-pi 5*pi], [r0 z0]);
+r = RZ(:,1)
+z = RZ(:,2)
+plot(r, z, '-');
+
+r0 = 0.5 * a;
+[V, RZ] = ode45(@fp, [-pi 5*pi], [r0 z0]);
+r = RZ(:,1)
+z = RZ(:,2)
+plot(r, z, '-');
